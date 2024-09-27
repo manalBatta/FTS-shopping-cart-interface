5bc851ed3c8e04463741f4fd55c7933083c1742c
manalBatta <95074111+manalBatta@users.noreply.github.com>
2024-09-27 12:37:07 +0300

    Implement Cart Details & Total Amount

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,19 +1,31 @@
 import styles from "./Cart.module.css";
 
 export default function Cart({ cartItems, handleDelete }) {
+  let totalPrice = 0;
   return (
-    <ul className={styles.cart}>
-      {cartItems.map((item) => {
-        if ("selected" in item && item.selected)
-          return (
-            <li
-              className={styles.cartItem}
-              key={item.id}
-              onClick={() => handleDelete(item.id)}>
-              {item.title}
-            </li>
-          );
-      })}
-    </ul>
+    <>
+      <ul className={styles.cart}>
+        {cartItems.map((item) => {
+          if ("selected" in item && item.selected) {
+            totalPrice += item.price;
+            return (
+              <li
+                className={styles.cartItem}
+                key={item.id}
+                onClick={() => handleDelete(item.id)}>
+                {item.title}
+                <br />
+                <strong>{item.price}$</strong>
+              </li>
+            );
+          }
+        })}
+        <li
+          className={styles.cartItem}
+          style={{ width: "fit-content", background: "#5ac1ff" }}>
+          Total Price = {Math.round(totalPrice)}$
+        </li>
+      </ul>
+    </>
   );
 }
